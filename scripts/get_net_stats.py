baa14d3b7f7f281429074b7fe4c11352b10b740e
Martin <martinperezmaneiro@gmail.com>
2022-06-24 12:32:58 +0200

    Correct previous script

--- scripts/get_net_stats.py
+++ scripts/get_net_stats.py
@@ -15,10 +15,11 @@ from next_sparseconvnet.utils.blob_classification_utils import *
 if __name__ == "__main__":
 
     config = configure(sys.argv).as_namespace
-    filein = os.path.expandvars(config.orig_file)
-    fileout = os.path.expandvars(config.pred_file)
+    orig_file = os.path.expandvars(config.orig_file)
+    pred_file = os.path.expandvars(config.pred_file)
+    out_file  = os.path.expandvars(config.out_file)
 
-    if os.path.isfile(fileout):
+    if os.path.isfile(out_file):
         raise Exception('output file exists, remove it manually')
 
     nevents = config.nevents
@@ -35,10 +36,10 @@ if __name__ == "__main__":
         for dis in distances:
             print(round(th, 4), round(dis, 4))
             start_time = time()
-            eventInfo_pred = segmentation_blob_classification(filein, fileout, th, nevents = nevents, max_distance = dis)
+            eventInfo_pred = segmentation_blob_classification(orig_file, pred_file, th, nevents = nevents, max_distance = dis)
             acc, tpr, tnr = success_rates(eventInfo_pred['binclass'], eventInfo_pred['pred_class'])
             result_df = result_df.append(pd.DataFrame([[th, dis, acc, tpr, tnr]], columns = ['blobclass_th', 'blobcount_dis', 'acc', 'tpr', 'tnr']))
             print((time() - start_time) / 60, 'mins')
-            
-    with tb.open_file(fileout, 'a') as h5out:
+
+    with tb.open_file(out_file, 'a') as h5out:
         dio.df_writer(h5out, result_df, 'NN_RESULTS', 'Statistics')
