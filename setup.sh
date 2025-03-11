0f4a87a86468d8f650d9d5457c1fcfe7c7f33256
Martin <martinperezmaneiro@gmail.com>
2025-03-11 17:56:31 +0100

    setup.sh generalization

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
-export SCN=$PWD
+export SCN=$( cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 ; pwd -P )/
 export PYTHONPATH=$SCN:$PYTHONPATH
 export TEST_DATA=$SCN//next_sparseconvnet/test_files/
+export PATH=$SCN/bin:$PATH
 export PATH=$SCN/scripts:$PATH
